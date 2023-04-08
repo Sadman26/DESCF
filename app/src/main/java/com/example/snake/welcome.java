f2bea24e7dbeab094e3f39e3a8dbaa239e87ec47
Sadman26 <fareless.ranger@gmail.com>
2023-04-09 01:16:09 +0600

    some little change

--- app/src/main/java/com/example/snake/welcome.java
+++ app/src/main/java/com/example/snake/welcome.java
@@ -12,12 +12,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class welcome extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     EditText loginkey;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
     ImageView loginbtn;
 
     @Override
@@ -26,6 +28,12 @@ public class welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         loginkey = findViewById(R.id.loginkey);
         loginbtn = findViewById(R.id.loginbtn);
+        //stay logged in
+        if(auth.getCurrentUser() != null){
+            Intent intent = new Intent(welcome.this, choose.class);
+            startActivity(intent);
+            finish();
+        }
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,9 +41,24 @@ public class welcome extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(welcome.this, "Login Successful 💚", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(welcome.this, choose.class);
-                            startActivity(intent);
+                            auth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        if(task.getResult().getUser().getUid().equals(task.getResult().getUser().getUid())){
+                                            Intent intent = new Intent(welcome.this, choose.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        else{
+                                            Toast.makeText(welcome.this, "Upps Login Failed!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                    else{
+                                        Toast.makeText(welcome.this, "Upps Login Failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                         }
                         else{
                             Toast.makeText(welcome.this, "Upps Login Failed!", Toast.LENGTH_SHORT).show();
