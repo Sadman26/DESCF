6355c21721afbd90c243b1714012c691a3e3e349
Sadman26 <fareless.ranger@gmail.com>
2023-04-09 03:31:57 +0600

    code to release done

--- app/src/main/java/com/example/snake/givecode.java
+++ app/src/main/java/com/example/snake/givecode.java
@@ -1,5 +1,6 @@
 package com.example.snake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,8 +9,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class givecode extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     EditText givecode;
     ImageView givecodeimgbtn;
 
@@ -18,21 +27,38 @@ public class givecode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_givecode);
+        DatabaseReference rescueRef = FirebaseDatabase.getInstance().getReference("rescue");
         givecode = findViewById(R.id.givecodetxt);
         givecodeimgbtn = findViewById(R.id.givecodebtn);
         givecodeimgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(givecode==null){
-                    givecode.setError("Please enter the code");
-                }
-                if(givecode.getText().toString().equals("")){
-                    givecode.setError("Please enter the code");
-                }
-                if(givecode.getText().toString().equals("1234")){
-                    Intent intent = new Intent(givecode.this,release.class);
-                    startActivity(intent);
-                }
+                rescueRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // Retrieve the children nodes
+                        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                            String childKey = childSnapshot.getKey();
+                            // Compare each child node with the user input
+                            if (childKey.equals(givecode.getText().toString())) {
+                                // If a match is found, start the desired activity or intent
+                                Intent intent = new Intent(givecode.this, release.class);
+
+                                startActivity(intent);
+                                break;
+                            }
+                            else{
+                                // If no match is found, display a toast message
+                                Toast.makeText(givecode.this, "Upps No Rescue!☺️", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
     }
