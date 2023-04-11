078a5b36af73a6b356f186cb80f03b32584bbc9d
Sadman26 <fareless.ranger@gmail.com>
2023-04-12 04:30:28 +0600

    added history and it works!

--- app/src/main/java/com/example/snake/history.java
+++ app/src/main/java/com/example/snake/history.java
@@ -1,14 +1,50 @@
 package com.example.snake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class history extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("rescue");
+    EditText historytxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        historytxt = findViewById(R.id.historytxt);
+        StringBuilder sb = new StringBuilder();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                    String snakeCode = childSnapshot.child("snakecode").getValue(String.class);
+                    String snakeId = childSnapshot.child("snakeid").getValue(String.class);
+                    sb.append("Snake Code: ");
+                    sb.append(snakeCode);
+                    sb.append("\n");
+                    sb.append("Snake ID: ");
+                    sb.append(snakeId);
+                    sb.append("\n");
+                    sb.append("\n");
+                    sb.append("\n");
+                }
+                historytxt.setText(sb.toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
